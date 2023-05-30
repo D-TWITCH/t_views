4d4d541c535fbfc66c4d2a6bfb8d19a90f65ce66
D-TWITCH <133078913+D-TWITCH@users.noreply.github.com>
2023-05-30 12:41:05 -0700

    Update chromedriver.py

--- chromedriver.py
+++ chromedriver.py
@@ -8,7 +8,10 @@ class ChromeDriver:
         options.add_argument('--no_sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--headless')
-
+        options.add_argument('--disable-gpu')
+        options.add_argument("start-maximized")
+        options.addArguments("disable-infobars")
+        
         self.driver = webdriver.Chrome(options=options)
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
