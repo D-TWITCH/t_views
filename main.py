720c815c677322475622db6b6e8aa9b59c0b1c05
D-TWITCH <133078913+D-TWITCH@users.noreply.github.com>
2023-05-30 13:13:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import random
 import multiprocessing
 import time
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
+import chromedriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
